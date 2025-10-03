aec222336c3c176b8735310d4d32e697aa0a7874
ADAM ANINI <adam.anini@nomachine.univ-eiffel.fr>
2025-10-03 11:31:20 +0200

    classes jetons et Ratelier

--- jetons.py
+++ jetons.py
@@ -1,1 +1,35 @@
-#gère les joueurs
+class Jeton:
+    def __init__(self, color: str, x: int, y: int) -> None:
+        self.color = color
+        self.x = x
+        self.y = y
+        
+        self.cachee = False
+        self.capture = False        
+
+    def retourner(self) -> None:
+        self.cachee = True
+
+    def capturer(self) -> None:
+        """ Ne devrait être appeller que si self.cachee est True """
+        self.capture = True
+
+
+class Ratelier:
+    def __init__(self, taille_max: int = 5) -> None:
+        self.taille_max = taille_max
+        self.jetons: list = []
+
+    def est_complet(self) -> bool:
+        return len(self.jetons) == self.taille_max
+
+    def triplette(self) -> str:
+        """
+        Si une triplette est formée, renvoie sa couleur,
+        None sinon.
+        """
+        ...
+
+    def est_vide(): ...
+    
+    def ajouter_jeton(self, jeton: Jeton): ...
